c96a304d10c76c37ae0c7584dddd494f4bb9be9e
Asma <asmalokhandwala2@gmail.com>
2019-12-03 16:11:47 -0400

    folders moved

---
+++ functions/functions/index.js
@@ -0,0 +1,18 @@
+const functions = require("firebase-functions");
+const stripe = require("stripe")("sk_test_ZxdMLN8pmsLfOyF4u8WIeKuk00LXy4AZIG");
+
+exports.Charge = functions.https.onRequest(async (req, res) => {
+  const token = req.body.stripeToken; // Using Express
+  const totalAmount = parseInt(req.body.amount);
+  return charge(totalAmount, token);
+});
+
+async function charge(amount, token) {
+  const charge = await stripe.charges.create({
+    amount: amount,
+    currency: "cad",
+    description: "Example charge",
+    source: token
+  });
+  return charge;
+}
