e16043b73bad424eba639e2df21cdc6e1216e87a
Asma <asmalokhandwala2@gmail.com>
2019-12-09 17:00:51 -0400

    Footer note added

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -11,6 +11,12 @@
           {{ new Date().getFullYear() }}
           <strong>Furniture You Rent</strong>
         </v-col>
+        <v-col class="text-center" cols="12">
+          <div class="font-weight-light">
+            Furniture You Rent is a fictional company created for the purpose of
+            making an e-commerce website as part of the university course COMP4983
+          </div>
+        </v-col>
       </v-row>
     </v-footer>
   </div>
